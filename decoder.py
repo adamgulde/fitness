6c00e003588772ba359d4d350dc2d4ddd575ae2d
adamgulde <adam.j.gulde@gmail.com>
2022-09-23 10:25:33 -0400

    working video to b64 string constantly

--- decoder.py
+++ decoder.py
@@ -3,7 +3,8 @@ import base64
 def decode(dataURL):
     with open("image_decoded.png", "wb") as save:
         save.write(base64.b64decode(dataURL))
-    ### Lower code used if text is manually saved to file
+
+    ##Lower code used if text is manually saved to file
     # img_data = ''
     # with open('data.txt', 'r') as data: ### Gets base64 string from data.txt in directory
     #     img_data = data.readline() 
--- extract.py
+++ extract.py
@@ -1,3 +1,19 @@
 from selenium import webdriver
 driver = webdriver.Chrome('/Users/2023148/Downloads/chromedriver')
-driver.get("https://www.google.com")
+from decoder import decode
+driver.get("http://127.0.0.1:8080/index.html") ### URL 
+
+### VERIFICATION BLOCK
+### 
+###
+###
+###
+###
+
+#canvas = driver.find_element_by_css_selector("#canvas")
+
+# get the canvas as a PNG base64 string
+#canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas)
+
+# decode
+#canvas_png = decode(canvas_base64)
