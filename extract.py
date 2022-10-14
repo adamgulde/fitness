38d5526a1bf49c45e53e1d22a3a9d90dfe66e03e
adamgulde <adam.j.gulde@gmail.com>
2022-10-14 11:08:48 -0400

    added drive and other cleanups plus note

--- extract.py
+++ extract.py
@@ -1,21 +1,27 @@
 ### Ensure Chrome is up to date, as is Selenium Chrome Webdriver
 
-from venv import create
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from decoder import decode
 import subprocess
+import os
 from ui import create_ui
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) ### Should work universally
 
-# driver = webdriver.Chrome('/Users/2023148/Downloads/chromedriver') UNCOMMENT ON MAC
-# driver = webdriver.Chrome() #executable_path="C:/selenium/chromedriver.exe") UNCOMMENT ON PC
+import platform
+if(platform.platform().startswith('macOS')):
+    ### MAC OS CODE
+    print('macOS System')
+    driver = webdriver.Chrome('/Users/2023148/Downloads/chromedriver')
+if(platform.platform().startswith('Windows')):
+    ### Windows OS CODE
+    print('Windows System')
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) ### Should work universally
+    # driver = webdriver.Chrome() #executable_path="C:/selenium/chromedriver.exe") UNCOMMENT ON PC
 
 def run():
     initialize()
     image_loop()
-
 
 def initialize():
     driver.get("https://adamgulde.github.io/")
@@ -25,9 +31,8 @@ def initialize():
     while(True):
         with open('cmds.txt', 'r') as cmd:
             if cmd.readline() == 'start': break
+    os.makedirs('')
     
-            
-
 def verification():
     ### VERIFICATION BLOCK
     ### 
@@ -45,14 +50,6 @@ def image_loop():
                 decode(driver.find_element('id', 'data_text').text, 2, count)
             else: break
         count+=1
-
-    #canvas = driver.find_element_by_css_selector("#canvas")
-
-    # get the canvas as a PNG base64 string
-    #canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas)
-
-    # decode
-    #canvas_png = decode(canvas_base64)
 
 if __name__=="__main__":
     run()
