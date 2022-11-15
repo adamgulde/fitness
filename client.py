65ecab36f9521be2c1b65c5a9db3ece94a8813c4
adamgulde <adam.j.gulde@gmail.com>
2022-11-15 09:34:15 -0500

    changing to local network hosting so much easier

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('192.168.100.31', 3131))
+
+image_to_upload = open('photo.jpg', 'rb')
+image_chunk = image_to_upload.read(2048)
+while image_chunk:
+    client_socket.send(image_chunk)
+    image_chunk = image_to_upload.read(2048)
+image_to_upload.close()
+
+
+image_to_save = open('client_save.jpg', 'wb')
+server_data = client_socket.recv(2048)
+while server_data:
+    image_to_save.write(server_data)
+    server_data = client_socket.recv(2048)
+image_to_save.close()
+
+# ### Client sends photo to server
+# file = open('photo.jpg', 'rb')
+
+# image_data = file.read(2048)
+
+# while image_data:
+#     clientSocket.send(image_data)
+#     image_data = file.read(2048)
+
+# file.close()
+client_socket.close()
--- modify.py
+++ modify.py
@@ -3,60 +3,52 @@
 
 import cv2
 import mediapipe as mp
-import 
 import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
-# For static images:
-IMAGE_FILES = []
-with mp_holistic.Holistic(
-    static_image_mode=True,
-    model_complexity=2,
-    enable_segmentation=True,
-    refine_face_landmarks=True) as holistic:
-    file = 'photo.jpg'
-    image = cv2.imread(file)
-    image_height, image_width, _ = image.shape
-    # Convert the BGR image to RGB before processing.
-    results = holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+# # For static images:
+# IMAGE_FILES = []
+# with mp_holistic.Holistic(
+#     static_image_mode=True,
+#     model_complexity=1,
+#     enable_segmentation=True,
+#     refine_face_landmarks=True) as holistic:
+#     file = 'photo.jpg'
+#     image = cv2.imread(file)
+#     image_height, image_width, _ = image.shape
+#     # Convert the BGR image to RGB before processing.
+#     results = holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    if results.pose_landmarks:
-      print(
-          f'Nose coordinates: ('
-          f'{results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].x * image_width}, '
-          f'{results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].y * image_height})'
-      )
+#     if results.pose_landmarks:
+#       print(
+#           f'Nose coordinates: ('
+#           f'{results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].x * image_width}, '
+#           f'{results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].y * image_height})'
+#       )
 
-    annotated_image = image.copy()
-    # Draw segmentation on the image.
-    # To improve segmentation around boundaries, consider applying a joint
-    # bilateral filter to "results.segmentation_mask" with "image".
-    condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-    bg_image = np.zeros(image.shape, dtype=np.uint8)
-    bg_image[:] = mp.BG_COLOR
-    annotated_image = np.where(condition, annotated_image, bg_image)
-    # Draw pose, left and right hands, and face landmarks on the image.
-    mp_drawing.draw_landmarks(
-        annotated_image,
-        results.face_landmarks,
-        mp_holistic.FACEMESH_TESSELATION,
-        landmark_drawing_spec=None,
-        connection_drawing_spec=mp_drawing_styles
-        .get_default_face_mesh_tesselation_style())
-    mp_drawing.draw_landmarks(
-        annotated_image,
-        results.pose_landmarks,
-        mp_holistic.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.
-        get_default_pose_landmarks_style())
-    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
-    # Plot pose world landmarks.
-    mp_drawing.plot_landmarks(
-        results.pose_world_landmarks, mp_holistic.POSE_CONNECTIONS)
+#     annotated_image = image.copy()
+#     # Draw segmentation on the image.
+#     # To improve segmentation around boundaries, consider applying a joint
+#     # bilateral filter to "results.segmentation_mask" with "image".
+#     # condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+#     # bg_image = np.zeros(image.shape, dtype=np.uint8)
+#     # bg_image[:] = mp.BG_COLOR
+#     # annotated_image = np.where(condition, annotated_image, bg_image)
+#     # Draw pose, left and right hands, and face landmarks on the image.
+#     mp_drawing.draw_landmarks(
+#         annotated_image,
+#         results.pose_landmarks,
+#         mp_holistic.POSE_CONNECTIONS,
+#         landmark_drawing_spec=mp_drawing_styles.
+#         get_default_pose_landmarks_style())
+#     cv2.imwrite('/tmp/annotated_image' + '.png', annotated_image)
+#     # Plot pose world landmarks.
+#     mp_drawing.plot_landmarks(
+#         results.pose_world_landmarks, mp_holistic.POSE_CONNECTIONS)
 
-### MAC CODE
+# ### MAC CODE
 
 
 
@@ -76,4 +68,11 @@ with mp_holistic.Holistic(
 
 
 
-### WINDOWS CODE
+# ### WINDOWS CODE
+
+def modify_image(image):
+    cv_image = cv2.imread(image)
+    cv_image = cv2.circle(cv_image, (100,100), 100, (255,0,0), 5)
+    cv2.imwrite('MOD'+image, cv_image)
+    
+modify_image('photo.jpg')
